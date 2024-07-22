a4b67facd291fe2a2cbb518ec3308eebc52f7f44
Peter Bartosh <petr.bartash@leverx.com>
2024-07-22 14:38:40 +0300

    deployment fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,15 +11,8 @@ plugins {
 group = "com.example"
 version = "0.0.1"
 
-//application {
-//    mainClass.set("io.ktor.server.netty.EngineMain")
-//
-//    val isDevelopment: Boolean = project.ext.has("development")
-//    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
-//}
-
 application {
-    mainClass.set("ServerKt")
+    mainClass.set("Server")
 }
 
 repositories {
